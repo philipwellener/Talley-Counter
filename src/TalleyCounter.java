5f9f84fa4b7b5801dbdf29430fd310a93b1105f2
Philip <Philip@172.20.10.2>
2018-09-26 11:09:13 -0400

    Yeet

--- src/TalleyCounter.java
+++ src/TalleyCounter.java
@@ -1,32 +1,50 @@
 /**
  * 
  * @author Philip Wellener
+ * Period 7
+ * AP Comp Sci
  *
  */
 
-public class TalleyCounter 
+public class TalleyCounter
 {
 	// Instance Variables
-	
 	private int count;
 	
+
 	// Constructor
-	
-	public TalleyCounter ()
+	public TalleyCounter()
 	{
 		count = 0;
+		
 	}
 	
-	public TalleyCounter (int startNum)
+	public TalleyCounter(int startNum)
 	{
 		count = startNum;
 		
 	}
-	
+
 	public void click()
 	{
 		count = count + 1;
-		//count++;
-		//count+=1;
+		//count++; count=count+1
+		//count+=1; count=count+1
+	}
+	
+	public void reset()
+	{
+		count = 0;
+	}
+	
+	public int getCount()
+	{
+		return count;
+	}
+	
+	public void unClick()
+	{
+		count = count - 1;
+				
 	}
 }
